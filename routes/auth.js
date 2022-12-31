f64ff7fdef7982d925904f956d70d1357864c7ac
manish <manishkumar60708090@gmail.com>
2022-12-31 19:16:00 +0530

    Auth check middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
 const router = express.Router();
+// import middleware
+const { authCheck } = require("../middlewares/auth");
 
+// controller
 const { createOrUpdateUser } = require("../controllers/auth");
+const myMiddleware = (req, res, next) => {
+  console.log("i am middleware");
+  next();
+};
 
-router.get("/create-or-update-user", createOrUpdateUser);
-
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+router.get("/testing", myMiddleware, (req, res) => {
+  res.json({
+    data: "you are successfully",
+  });
+});
 module.exports = router;
